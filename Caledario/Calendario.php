7e8299d989cfe17d9d7b745f1962506f0910fdde
NildsonKleyton <nildsonkleyton@hotmail.com>
2023-07-17 14:51:13 -0300

    Mes / Ano

--- Caledario/Calendario.php
+++ Caledario/Calendario.php
@@ -18,7 +18,7 @@
             </div>
             <div class="center">
                 <?php
-                echo "Mês";
+                echo "Mês / ano;"
 
                 function diaSemana()
                 {
@@ -30,15 +30,19 @@
                     $diaSemana .= "</tr>";
                     return $diaSemana;
                 }
+
                 function mes($valor)
                 {
                     $diaMes = "<tr>";
                     for ($i = 1; $i <= $valor; $i++) {
-                        if ($i % 7 == 0) $diaMes .= "<td> $i </td></tr>";
-                        else $diaMes .= "<td> $i </td>";
+                        if ($i % 7 == 0) 
+                            $diaMes .= "<td> $i </td></tr>";
+                        else
+                            $diaMes .= "<td> $i </td>";
                     }
                     return $diaMes;
                 }
+                
                 echo "<table>" . diaSemana(). mes(31)."</table>";
 
                 ?>
